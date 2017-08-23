87b958486e1a2cfa10af68725245534ad408215d
codewithdj <deepak_jain@persistent.com>
2017-08-23 16:45:00 +0530

    EOD commit

--- index.php
+++ index.php
@@ -34,9 +34,9 @@ $siteNameCheck = preg_match('~^[A-Za-z0-9_]{3,20}$~i', $sitePostName);
   	$result=mysqli_query($conn,$sql);
     	$count = mysqli_num_rows($result);
                     if($count == 1){
-                        header("Location: messages.php");
+                        header("Location: feedback.php");
                     }else{
-                        echo "Username Not Available";
+                        echo "User Not Available";
                     }
         	
    }
@@ -46,62 +46,3 @@ $siteNameCheck = preg_match('~^[A-Za-z0-9_]{3,20}$~i', $sitePostName);
    }
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-  
-    <title>FarqPadtaHai</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="starter-template.css" rel="stylesheet">
-  </head>
-
-  <body>
-
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-      <a class="navbar-brand" href="index.php">FarqPadtaHai</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="signup.php">Register <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="login.php">Login</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">About Us</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Contact Us</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-
-    <div class="container">
-
-      <div class="starter-template">
-        <h5>Get honest feedback from your coworkers and friends</h5>
-      </div>
-
-    </div><!-- /.container -->
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="js/jquery-3.2.1.js"  crossorigin="anonymous"></script>
-    </body>
-</html>
--- login.php
+++ login.php
@@ -1,4 +1,13 @@
-
+<?php
+	session_start();
+	if(isset($_SESSION['ulogin']))
+      {
+	     if($_SESSION['ulogin'] == "yes"){
+			 header("location: dashboard.php");
+			 exit();
+		 }
+      }
+	?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -22,17 +31,40 @@
 
     <div class="container">
 
-      <form class="form-signin">
+      <form method="POST" class="form-signin">
         <h2 class="form-signin-heading">Please sign in</h2>
-        <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <label for="username" class="sr-only">Username</label>
+        <input type="text" id="username" name="username" class="form-control" placeholder="Username" required autofocus>
         <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <input type="password" name="password" id="password" class="form-control" placeholder="Password" required>
         <div class="checkbox">
           <label>
             <input type="checkbox" value="remember-me"> Remember me
           </label>
         </div>
+        
+          
+	           <?php
+             include("dbConnect.php");
+              if((isset($_POST["username"]) && isset($_POST["password"])) ){
+                $username= $_POST["username"];
+                $password = $_POST["password"];
+
+                $query ="select * from users where u_name='$username' and pwd='$password'";
+                $result = mysqli_query($conn,$query);
+                
+                if(mysqli_num_rows($result)>0){
+                  header("location: dashboard.php");
+                  //   header("location:".$username.".farqpadtahai.com/dashboard.php");
+                    $_SESSION["ulogin"] = "yes";
+                }
+                else{
+                echo '<p class="h2" style="color:red"> Login Failed</p>';
+                }
+             }	 
+	          ?>
+
+
         <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
       </form>
 
